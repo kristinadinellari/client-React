4610f67397b8f31db13814db24c1eb209a84e4ff
Kristina Dinellari <kristinadinellari@gmail.com>
2020-01-16 16:49:00 +0100

    --refac: sidebar and nav component

--- src/components/Menu/Nav.tsx
+++ src/components/Menu/Nav.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React from 'react';
+import clsx from 'clsx';
+import useStyles from './Style';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-class Nav extends React.Component {
-  render() {
-    return (
-      <div className="navComponent">
-      </div>
-    );
-  }
+export default function Nav(props: any) {
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed"
+        className={clsx(classes.appBar, { [classes.appBarShift]: props.open })}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={props.handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, props.open && classes.hide)}>
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
-export default Nav;
--- src/components/Menu/Sidebar.tsx
+++ src/components/Menu/Sidebar.tsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import useStyles from './Style';
+import { useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import PeopleIcon from '@material-ui/icons/People';
 
-class Sidebar extends React.Component {
-  render() {
-    return (
-      <div className="sidebarComponent">
-        <div id="Sidebar">
-          <h1>this  Sidebar component</h1>
-          hello
-          </div>
-      </div>
-    );
-  }
+export default function Sidebar(props: any) {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Drawer className={classes.drawer} variant="persistent"
+        anchor="left" open={props.open} classes={{ paper: classes.drawerPaper }}>
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={props.handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {[{ title: 'Profile', route: '/profile', icon: '' }, { title: 'Users', route: '/users' }].map((text, index): any => (
+            <NavLink to={text.route} key={index}>
+              <ListItem button >
+                <ListItemIcon>{index % 2 === 0 ? <PermIdentityIcon /> : <PeopleIcon />}</ListItemIcon>
+                <ListItemText primary={text.title} />
+              </ListItem>
+            </NavLink>
+          ))}
+        </List>
+      </Drawer>
+    </div>
+  );
 }
-
-export default Sidebar;
